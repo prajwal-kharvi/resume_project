4c58115f336171596bf82d88347320b218c74c2d
prajwal_kharvi <kharviprajwalp@gmail.com>
2026-01-16 16:35:50 +0530

    update About file(CV)

--- my_resume/src/components/About/About.js
+++ my_resume/src/components/About/About.js
@@ -48,7 +48,8 @@ const About = () => {
                             an entry-level position to contribute to innovative projects.
                         </p>
                     {/*Resume button*/}
-                    <a href="https://drive.google.com/file/d/1Kd0Z4gJq3gi1gZVy0-nfGtCvARV9mubn/view?usp=sharing"
+
+                    <a href={process.env.REACT_APP_CV_LINK}
                     target="_blank" rel="noopener noreferrer"
                        className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
                     style={{
